30f56a4c7cb4215b061269dea04805e9c27d0e7a
D.Filonenko <fildv7@gmail.com>
2017-11-22 12:01:32 +0200

    created structure app in system tray. Created timer and text file reader.

---
+++ EducationHelper/FileReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace EducationHelper
+{
+    public static class FileReader
+    {
+        static readonly Random _rnd = new Random();
+        static readonly object _syncLock = new object();
+
+        public static int GetRandomNumber(int max)
+        {
+            lock (_syncLock)
+            { // synchronize
+                return _rnd.Next(0, max);
+            }
+        }
+
+        static List<string> ReadTask(string fileName)
+        {
+            List<string> resList = new List<string>();
+
+            using (var streamReader = File.OpenText(fileName))
+            {                
+                var tasks = streamReader.ReadToEnd().Split(',');
+                int rnd = GetRandomNumber(tasks.Count());
+
+                string task = tasks[rnd];
+                string[] lines = streamReader.ReadToEnd().Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+                foreach (var line in lines)
+                {
+                    resList.Add(line.Trim());
+                }
+            }
+            return resList;
+        }
+    }
+}
---
+++ EducationHelper/MyCustomApplicationContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EducationHelper
+{
+    class MyCustomApplicationContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+        private System.Timers.Timer _timer;
+
+        public MyCustomApplicationContext()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = new Icon("..\\..\\img\\spain_flag.ico"),
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                                                    new MenuItem("Settings", Settings),
+                                                    new MenuItem("Exit", Exit)          
+                                                              }),
+                Visible = true
+            };
+
+            StartTimer();
+        }       
+
+        void Settings(object sender, EventArgs e)
+        {
+           
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        void StartTimer()
+        {
+            _timer = new System.Timers.Timer();
+            _timer.Enabled = true;
+            _timer.Interval = 10000; // 10sec
+            _timer.Elapsed += new System.Timers.ElapsedEventHandler(this.timer_Elapsed);
+            _timer.AutoReset = true;
+            _timer.Start();
+        }
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            MessageBox.Show("Hola de tray!");
+        }
+    }
+}
